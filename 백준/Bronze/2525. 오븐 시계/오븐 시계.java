35bab3284979fa0276c76acaaed5a6d5be513f58
JeonSeungyeon <101554805+JeonSeungyeon@users.noreply.github.com>
2024-07-10 22:28:28 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18560 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int h = sc.nextInt();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
         int m = sc.nextInt();
         int t = sc.nextInt();
+        
+        h = h + (t + m)/60;
+        m = (t + m)%60;
+        
+        if(h >= 24) {
+        	h -= 24;
+        }
+        
+        System.out.println(h + " " +m);
+	}
 
-        h += t / 60;
-        m += t % 60;
-        
-        if(m >= 60){
-            h += 1;
-            m -= 60;
-        }
-        if(h >= 24){
-            h -= 24;
-        }
-        
-        System.out.println(h + " " + m);
-    }
 }
