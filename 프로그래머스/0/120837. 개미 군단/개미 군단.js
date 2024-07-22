3a32d33ae46921a34d6f234b784be9b9d6d58d78
JeonSeungyeon <101554805+JeonSeungyeon@users.noreply.github.com>
2024-07-22 18:36:13 +0900

    [level 0] Title: 개미 군단, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,11 @@
+function solution(hp) {
+    var answer = 0;
+    ant1 = 0;
+    ant2 = 0;
+    ant3 = 0;
+    ant1 = Math.trunc(hp/5)
+    ant2 = Math.trunc(hp%5/3)
+    ant3 = Math.trunc(hp%5%3)
+    answer = ant1 + ant2 + ant3
+    return answer;
+}
