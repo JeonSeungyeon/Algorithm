ef712ecea357fcffe7cc333f73654f4011a36570
JeonSeungyeon <101554805+JeonSeungyeon@users.noreply.github.com>
2024-09-03 20:07:03 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.48 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,13 @@
+function solution(arr, flag) {
+    var x = [];
+    for(i = 0; i < arr.length; i++) {
+        if(flag[i]) {
+            for(j = 0; j < arr[i]*2; j++) {
+                x.push(arr[i])
+            }
+        } else {
+            x.splice(-arr[i], arr[i])
+        }
+    }
+    return x;
+}
