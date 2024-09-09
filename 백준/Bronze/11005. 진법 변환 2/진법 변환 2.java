fc48c6bd3b609087cad205f83c0e2023c52df6de
JeonSeungyeon <101554805+JeonSeungyeon@users.noreply.github.com>
2024-09-09 23:28:48 +0900

    [Bronze I] Title: 진법 변환 2, Time: 172 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        String result = Integer.toString(N, B).toUpperCase();
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
